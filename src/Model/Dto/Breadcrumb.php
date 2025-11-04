048e0fab8567728281b23ab7745deab391da5a4e
inwebo <github@inwebo.net>
2025-11-04 11:49:48 +0100

    Reverse to getters

--- src/Model/Dto/Breadcrumb.php
+++ src/Model/Dto/Breadcrumb.php
@@ -11,21 +11,20 @@ final class Breadcrumb
      * @param string      $url   Absolute URL for the breadcrumb
      * @param string|null $title Optional SEO title
      */
-    public function __construct(
-        public string  $name {
-            get {
-                return $this->name;
-            }
-        },
-        public string  $url {
-            get {
-                return $this->url;
-            }
-        },
-        public ?string $title {
-            get {
-                return $this->title;
-            }
-        },
-    ) {}
+    public function __construct(private string $name, private string $url, private ?string $title = null) {}
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 }
